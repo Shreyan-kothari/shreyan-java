32b3b45ce417b6e303aa8a65aa080324c7e94cff
Shreyan-kothari <68265557+Shreyan-kothari@users.noreply.github.com>
2020-10-16 21:13:24 +0530

    Add files via upload

---
+++ assignment1/employe.java
@@ -0,0 +1,20 @@
+package assignment1;
+
+public class employe {
+	
+	    int age;  
+	    String name;  
+	    String  city;  
+	    void func( String n,int a, String c) {  
+	        name=n;  
+	        age=a;  
+	        city=c;  
+	    }  
+	    void display(){
+	System.out.println("The name is "+name);
+	System.out.println("The age is "+age);
+	System.out.println("The city is "+city);}  
+	}  
+	 
+
+
---
+++ assignment1/employee.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+
+public class employee {
+
+	public static void main(String[] args) {
+		 employe e1=new employe();  
+		 employe e2=new employe();  
+		      
+		    e1.func("Shreyaan",22,"jaipur");  
+		    e2.func("Roshan",20,"jaipur");  
+		      
+		    e1.display();  
+		    e2.display();
+
+	}
+
+}
